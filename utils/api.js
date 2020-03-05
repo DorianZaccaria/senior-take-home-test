b337418de20467cb6b0e56296ed7e576950fc77f
Dorian Zaccaria <dorian.zaccaria@gmail.com>
2020-03-05 21:40:56 +0100

    [api] Small refacto for step 2
    This commit:
    * extracts the logic to check if a trial is ongoing in it's own function
    * factorises the trials filtering logic

--- utils/api.js
+++ utils/api.js
@@ -24,8 +24,26 @@ const formatTrials = (trials, callback) => {
   callback(null, formatedTrials);
 };
 
+const isTrialOngoing = trial => {
+  // The trial can't be ongoing if it is canceled
+  if (trial.canceled)
+    return false;
+
+  // The trial can't be ongoing if the start date is in the future or if the end
+  // date is in the past.
+  const today = new Date;
+  const start = new Date(trial.start_date);
+  const end = new Date(trial.end_date);
+
+  if (start > today || end < today)
+    return false;
+
+  return true;
+};
+
 // Exports
 module.exports = {
   formatTrials,
-  handleErrors
+  handleErrors,
+  isTrialOngoing
 };
